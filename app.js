580094078df3818f250284ff4cddbb3996e382c8
root <root@webserver>
2016-12-22 14:27:52 -0600

    Fixed issue with using variables in console.log.

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 var http = require('http');
-var PORT = process.env.NODE_PORT;
+var PORT = process.env.NODE_PORT || 3000;
 var ENV = process.env.NODE_ENV;
 
 http.createServer(function(req, res) {
-	console.log('Process ID: %d - Request received', process.pid);
+	console.log('Process ID: ' + process.pid + ' - Request received');
 	res.writeHead(200, { 'Content-Type': 'text/plain' });
-	res.end('Welcome to %s\n', ENV);
+	res.end('Welcome to ' + ENV);
 }).listen(PORT);
 
-console.log('Process ID: %d - listening on port %d', process.pid, PORT);
+console.log('Process ID: ' + process.pid + ' - listening on port ' + PORT);
